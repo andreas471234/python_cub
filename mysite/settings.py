dace5f77355fdecb4f10a681a8fb44e260cf848c
Andre Bismo <andrebismo@gmail.com>
2018-03-04 19:06:25 +0000

    setting in prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,8 +137,8 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
